7fc3165ff385c9de504cc4daffa861be3360b628
rupadevii <rupadevi7094@gmail.com>
2025-10-25 20:33:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const openModal = document.getElementById("openModal")
+const modal = document.querySelector(".modal")
+const closeModal = document.querySelector(".close-modal")
+
+openModal.addEventListener("click", () => {
+	modal.style.display = "block";
+	document.body.style.backgroundColor = "gray";
+})
+
+closeModal.addEventListener("click", () => {
+	modal.style.display = "none";
+	document.body.style.backgroundColor = "white"
+})
